f10204186b9e628972d688a8cc8bf92be85ff42e
m-grainger <grainger.pdx@gmail.com>
2023-04-24 12:14:51 -0700

    chore: updated icons

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -41,7 +41,7 @@ const ExperienceCard = ({ experience }) => {
           <li
             key={`experience-point-${index}`}
             className="text-white-100 text-[14px] pl-1 tracking-wider"
-            style={{ "list-style-type": "circle" }}
+            style={{ listStyleType: "circle" }}
           >
             {point}
           </li>
