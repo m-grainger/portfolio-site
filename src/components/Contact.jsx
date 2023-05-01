9051dcd690ce1868c96f7d023357988c869bc504
m-grainger <grainger.pdx@gmail.com>
2023-05-01 11:44:17 -0700

    fix: fixed bug in email.send, updated some values

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,18 +33,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        emailjs.send(
-          import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-          import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-          {
-            from_name: form.name,
-            to_name: "Matt",
-            from_email: form.email,
-            to_email: "grainger.pdx@gmail.com",
-            message: form.message,
-          },
-          import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-        )
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Matt",
+          from_email: form.email,
+          to_email: "grainger.pdx@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
@@ -100,7 +98,7 @@ const Contact = () => {
               name="email"
               value={form.email}
               onChange={handleChange}
-              placeholder="What's your web address?"
+              placeholder="What's your email address?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
