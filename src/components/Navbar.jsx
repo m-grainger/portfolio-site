b9fdc8c6d20a4a7aa299b693bb16f1da2399bcbc
m-grainger <grainger.pdx@gmail.com>
2023-05-02 10:20:27 -0700

    feat: added and updated favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
-import { logo, menu, close } from "../assets";
+import { mglogo1, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState('"');
@@ -20,7 +20,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <img src={mglogo1} alt="mglogo1" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Matt Grainger &nbsp;
             <span className=""> | &nbsp; Software Engineer</span>
